8f57e61571b027d044571531ff741e0b8f4c598e
definitly486 <fg8989@gmail.com>
2025-09-26 22:52:22 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,8 +10,8 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     QIcon icon("/usr/local/share/pixmaps/zapret.png");
     MainWindow w;
-    int screenWidth = QApplication::desktop()->screenGeometry().width();
-    int screenHeight = QApplication::desktop()->screenGeometry().height();
+    int screenWidth = QApplication::primaryScreen()->geometry().width();
+    int screenHeight = QApplication::primaryScreen()->geometry().height();
     int windowWidth = w.width();
     int windowHeight = w.height();
     int x = (screenWidth - windowWidth) / 2;
